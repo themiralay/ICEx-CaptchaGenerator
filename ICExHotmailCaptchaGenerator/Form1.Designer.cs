d13ff42abc939607f0a3100ff89722eebc047378
Vo-coder <53035239+Vo-coder@users.noreply.github.com>
2020-07-20 17:49:09 +0300

    Add project files.

---
+++ ICExHotmailCaptchaGenerator/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ICExHotmailCaptchaGenerator
+{
+    partial class startProcess
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getCaptcha = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.getCounter = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.folderPathShowBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folder = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getCounter)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // getCaptcha
+            // 
+            this.getCaptcha.Location = new System.Drawing.Point(227, 82);
+            this.getCaptcha.Name = "getCaptcha";
+            this.getCaptcha.Size = new System.Drawing.Size(86, 96);
+            this.getCaptcha.TabIndex = 0;
+            this.getCaptcha.Text = "Get One CAPTCHA";
+            this.getCaptcha.UseVisualStyleBackColor = true;
+            this.getCaptcha.Click += new System.EventHandler(this.getCaptcha_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(6, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(307, 54);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 96);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // getCounter
+            // 
+            this.getCounter.Location = new System.Drawing.Point(193, 83);
+            this.getCounter.Name = "getCounter";
+            this.getCounter.Size = new System.Drawing.Size(120, 23);
+            this.getCounter.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.getCaptcha);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(323, 190);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Test";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.folderPathShowBox);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.folder);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Controls.Add(this.getCounter);
+            this.groupBox2.Location = new System.Drawing.Point(12, 208);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(323, 145);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Locally Save ";
+            // 
+            // folderPathShowBox
+            // 
+            this.folderPathShowBox.Enabled = false;
+            this.folderPathShowBox.Location = new System.Drawing.Point(9, 44);
+            this.folderPathShowBox.Name = "folderPathShowBox";
+            this.folderPathShowBox.Size = new System.Drawing.Size(304, 33);
+            this.folderPathShowBox.TabIndex = 6;
+            this.folderPathShowBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Please Select Folder Path:";
+            // 
+            // folder
+            // 
+            this.folder.Location = new System.Drawing.Point(155, 15);
+            this.folder.Name = "folder";
+            this.folder.Size = new System.Drawing.Size(158, 23);
+            this.folder.TabIndex = 4;
+            this.folder.Text = "FOLDER";
+            this.folder.UseVisualStyleBackColor = true;
+            this.folder.Click += new System.EventHandler(this.folder_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(9, 112);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(311, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Developed By Enes Can IŞIK";
+            // 
+            // startProcess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 377);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "startProcess";
+            this.Text = "ICEx Hotmail CAPTCHA";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getCounter)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getCaptcha;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown getCounter;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox folderPathShowBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button folder;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ICExHotmailCaptchaGenerator/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace ICExHotmailCaptchaGenerator
+{
+    public partial class startProcess : Form
+    {
+        Uri mainURL = new Uri("https://eus.client.hip.live.com/GetHIP/GetHIPAMFE/HIPAMFE?dc=EUS&mkt=tr-TR&id=15041&fid=d5b9f8d61eea4c1ca206631bf391aa3c&type=visual&c=14&rnd=0.007418881013300238");
+        string folderPath;
+        string escapedURL;
+        string unescapedURL;
+        bool success = false;
+        bool nextones=true;
+        int counter;
+        public startProcess()
+        {
+            InitializeComponent();
+        }
+        public void Downloader(string uri)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    escapedURL = Between(reader.ReadToEnd(), "\"hipChallengeUrl\":\"", "\"");
+                    unescapedURL = Regex.Unescape(escapedURL);
+                    success = true;
+                    using (WebClient wc = new WebClient())
+                    {
+                        wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+                        wc.DownloadFileAsync(new Uri(unescapedURL), folderPath + "/" + "ICEx_" + DateTime.Now.ToString("yyyy-dd-M-HH-mm-ss") + ".png");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        public void Get(string uri)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    escapedURL = Between(reader.ReadToEnd(), "\"hipChallengeUrl\":\"", "\"");
+                    unescapedURL= Regex.Unescape(escapedURL);
+                    Invoke(new Action(() =>
+                    {
+                        richTextBox1.Text = unescapedURL;
+                    }));
+                    pictureBox1.LoadAsync(unescapedURL);
+                    success = true;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            
+        }
+        public string Between(string str, string firstString, string lastString)
+        {
+            int pos1 = str.IndexOf(firstString) + firstString.Length;
+            int pos2 = str.Substring(pos1).IndexOf(lastString);
+            return str.Substring(pos1, pos2);
+        }
+        private void getCaptcha_Click(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() => { Get(mainURL.ToString()); });
+        }
+        private void folder_Click_1(object sender, EventArgs e)
+        {
+            FolderBrowserDialog Folder = new FolderBrowserDialog();
+            Folder.ShowDialog();
+            folderPath = Folder.SelectedPath;
+            folderPathShowBox.Visible = true;
+            folderPathShowBox.Text = folderPath;
+        }
+        //private void downloadFile()
+        //{
+        //    for (int i = 0; i < getCounter.Value; i++)
+        //    {
+        //        using (WebClient wc = new WebClient())
+        //        {
+        //            wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+        //            wc.DownloadFileAsync(new Uri(unescapedURL), folderPath + "/" + "ICEx_" + DateTime.Now.ToString("yyyy-dd-M-HH-mm-ss")+".png");
+        //        }
+        //    }
+        //}
+        //private void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        //{
+        //    progressBar1.Value = e.ProgressPercentage;
+        //}
+        private void wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            nextones = true;
+            counter++;
+            Invoke(new Action(() =>
+            {
+                progressBar1.Value++;
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum =Convert.ToInt32(getCounter.Value)+1;
+            if (success)
+            {
+                if (folderPath != null)
+                {
+                    Task t =Task.Factory.StartNew(() =>
+                    {
+                        do
+                        {
+                            if (nextones)
+                            {
+                                nextones = false;
+                                Downloader(mainURL.ToString());
+                            }
+
+                        }
+                        while (counter!=getCounter.Value);
+                        Invoke(new Action(() =>
+                        {
+                            MessageBox.Show("Completed!");
+                            counter = 0;
+                            progressBar1.Value = 0;
+                        }));
+                    });
+                }
+            }
+            else
+                MessageBox.Show("please press the \"Get One CAPTCHA \" button for the first test.");
+        }
+    }
+}
